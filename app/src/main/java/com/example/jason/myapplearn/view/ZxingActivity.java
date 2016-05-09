f9362f50242c9db399eae36a177effe49496c58b
wangbf <wangbf@qq.com>
2016-05-09 13:40:32 +0800

    wangbf

---
+++ app/src/main/java/com/example/jason/myapplearn/view/ZxingActivity.java
@@ -0,0 +1,16 @@
+package com.example.jason.myapplearn.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jason.myapplearn.R;
+
+public class ZxingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zxing);
+
+    }
+}
