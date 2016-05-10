f94351d0daafa360148362ca195c62a2762b675c
wangbf <wangbf@qq.com>
2016-05-10 20:04:40 +0800

    Volley Test

--- app/src/main/java/com/example/jason/myapplearn/view/ZxingActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/ZxingActivity.java
@@ -2,21 +2,27 @@ package com.example.jason.myapplearn.view;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jason.myapplearn.R;
+import com.xys.libzxing.zxing.activity.CaptureActivity;
+import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
 public class ZxingActivity extends AppCompatActivity {
 
-    private Button btnScan,btnCreateScan;
-    private TextView txtViewResult;
-    private ImageView imgViewBarCode;
-    private final static int SCANNIN_GREQUEST_CODE = 1;
+    private TextView resultTextView;
+    private EditText qrStrEditText;
+    private ImageView qrImgImageView;
+    private CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,43 +30,54 @@ public class ZxingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_zxing);
 
         initView();
-        //点击按钮跳转到二维码扫描界面，这里用的是startActivityForResult跳转
-        //扫描完了之后调到该界面
-        btnScan.setOnClickListener(new View.OnClickListener() {
+        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
+        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
+        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
+        mCheckBox = (CheckBox) findViewById(R.id.logo);
+
+        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(ZxingActivity.this, CaptureActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
+                //打开扫描界面扫描条形码或二维码
+                Intent openCameraIntent = new Intent(ZxingActivity.this, CaptureActivity.class);
+                startActivityForResult(openCameraIntent, 0);
             }
         });
+
+        Button generateQRCodeButton = (Button) this.findViewById(R.id.btn_add_qrcode);
+        generateQRCodeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String contentString = qrStrEditText.getText().toString();
+                if (!contentString.equals("")) {
+                    //根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
+                    Bitmap qrCodeBitmap = EncodingUtils.createQRCode(contentString, 350, 350,
+                            mCheckBox.isChecked() ?
+                                    BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher) :
+                                    null);
+                    qrImgImageView.setImageBitmap(qrCodeBitmap);
+                } else {
+                    Toast.makeText(ZxingActivity.this, "Text can not be empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void initView(){
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case SCANNIN_GREQUEST_CODE:
-                if(resultCode == RESULT_OK){
-                    Bundle bundle = data.getExtras();
-                    //显示扫描到的内容
-                    txtViewResult.setText(bundle.getString("result"));
-                    //显示
-                    imgViewBarCode.setImageBitmap((Bitmap) data.getParcelableExtra("bitmap"));
-                }
-                break;
+        if (resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            resultTextView.setText(scanResult);
         }
-
-    }
-
-    private void initView(){
-        btnScan=(Button)findViewById(R.id.btnScan);
-        btnCreateScan=(Button)findViewById(R.id.btnCreateCode);
-        txtViewResult=(TextView)findViewById(R.id.txtScanResult);
-        imgViewBarCode=(ImageView)findViewById(R.id.imgViewBarCode);
     }
 
 }
