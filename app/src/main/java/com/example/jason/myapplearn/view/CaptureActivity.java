49917bec9a6372a7e5bd9339b23bc02b3a86d6ac
wangbf <wangbf@qq.com>
2016-05-10 16:47:36 +0800

    wangbf test

--- app/src/main/java/com/example/jason/myapplearn/view/CaptureActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/CaptureActivity.java
@@ -1,7 +1,11 @@
 package com.example.jason.myapplearn.view;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Interpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.jason.myapplearn.R;
 
@@ -12,7 +16,9 @@ public class CaptureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture);
 
-
+       // CameraManager.init(getApplication());
 
     }
+
+
 }
