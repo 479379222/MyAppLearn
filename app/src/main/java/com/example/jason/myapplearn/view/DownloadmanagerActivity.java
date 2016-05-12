9810b6f2ffebab4c94b0e8008ca6c3930367a2f8
wangbf <wangbf@qq.com>
2016-05-12 10:27:42 +0800

    wangbf test

--- app/src/main/java/com/example/jason/myapplearn/view/DownloadmanagerActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/DownloadmanagerActivity.java
@@ -2,16 +2,34 @@ package com.example.jason.myapplearn.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.jason.myapplearn.R;
 
 public class DownloadmanagerActivity extends AppCompatActivity {
+
+    private ImageView imgViewPic;
+    private Button btnDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_downloadmanager);
 
+        initView();
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+    }
+
+    private void initView(){
+        imgViewPic=(ImageView)findViewById(R.id.imgViewPic);
+        btnDownload=(Button)findViewById(R.id.btnDownload);
     }
 }
