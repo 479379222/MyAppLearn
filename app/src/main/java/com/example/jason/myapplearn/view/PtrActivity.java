762536a2d5e24d959c022b7113dafdddf63b482e
wangbf <479379222@qq.com>
2016-05-15 21:25:22 +0800

    wangbf update

--- app/src/main/java/com/example/jason/myapplearn/view/PtrActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/PtrActivity.java
@@ -2,14 +2,57 @@ package com.example.jason.myapplearn.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.jason.myapplearn.R;
+import com.example.jason.myapplearn.utils.ToastUtils;
+
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.PtrHandler;
 
 public class PtrActivity extends AppCompatActivity {
+
+    private PtrClassicFrameLayout mPtrFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptr);
+
+        mPtrFrame = (PtrClassicFrameLayout)findViewById(R.id.rotate_header_list_view_frame);
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                updateData();
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+
+        });
+        // the following are default settings
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        // default is false
+        mPtrFrame.setPullToRefresh(false);
+        // default is true
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
+        mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPtrFrame.autoRefresh();
+            }
+        }, 100);
+    }
+
+    private void updateData(){
+        ToastUtils.showLong(PtrActivity.this,"下拉刷新!");
     }
 }
