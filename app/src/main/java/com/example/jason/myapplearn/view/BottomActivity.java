fc79a9ad3e638b28d91efeac73d55732385cdf08
wangbf <479379222@qq.com>
2016-05-17 22:06:54 +0800

    wangbf update

--- app/src/main/java/com/example/jason/myapplearn/view/BottomActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/BottomActivity.java
@@ -4,14 +4,21 @@ import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 import com.example.jason.myapplearn.BaseActivity;
 import com.example.jason.myapplearn.R;
 import com.example.jason.myapplearn.common.SystemBarTintManager;
+import com.example.jason.myapplearn.utils.ToastUtils;
 
 public class BottomActivity extends AppCompatActivity {
+
+    private FrameLayout fragment_container;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +34,35 @@ public class BottomActivity extends AppCompatActivity {
 
         }*/
 
+        initView();
+        inflater = getLayoutInflater();
+        View v = inflater.inflate(R.layout.fragment_index,null);
+        fragment_container.addView(v);
 
+    }
 
+    private void initView() {
+        fragment_container = (FrameLayout) findViewById(R.id.fragment_container);
+    }
+
+    public void onTabClicked(View v) {
+        switch (v.getId()) {
+            case R.id.re_weixin:
+                ToastUtils.showLong(BottomActivity.this, "首页");
+                break;
+            case R.id.re_weixin1:
+                ToastUtils.showLong(BottomActivity.this, "活动");
+                break;
+            case R.id.re_weixin2:
+                ToastUtils.showLong(BottomActivity.this, "分类");
+                break;
+            case R.id.re_weixin3:
+                ToastUtils.showLong(BottomActivity.this, "购物车");
+                break;
+            case R.id.re_weixin4:
+                ToastUtils.showLong(BottomActivity.this, "我的");
+                break;
+        }
     }
 
     @TargetApi(19)
@@ -43,4 +77,5 @@ public class BottomActivity extends AppCompatActivity {
         }
         win.setAttributes(winParams);
     }
+
 }
