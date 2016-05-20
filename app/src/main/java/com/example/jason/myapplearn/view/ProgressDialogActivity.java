f89417667ce1497db479d99bfca76c57f714d737
wangbf <479379222@qq.com>
2016-05-20 09:22:34 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/jason/myapplearn/MainActivity.java

--- app/src/main/java/com/example/jason/myapplearn/view/ProgressDialogActivity.java
+++ app/src/main/java/com/example/jason/myapplearn/view/ProgressDialogActivity.java
@@ -16,6 +16,7 @@ public class ProgressDialogActivity extends AppCompatActivity {
     private Button button3, button4;
     private ProgressDialog pd1, pd2;
     private Context context;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +39,35 @@ public class ProgressDialogActivity extends AppCompatActivity {
             int id = v.getId();
             if (id == R.id.button3) {
                 pd1 = new ProgressDialog(context);
-
+                pd1 = ProgressDialog.show(context, "自动关闭对话框", "Working,,,,,,1秒", true, false);
+                Thread thread = new Thread();//开启一个线程来延时
+                thread.start();//启动线程
             } else {
-
+                pd1 = new ProgressDialog(context);// 创建ProgressDialog对象
+                pd1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);// 设置进度条风格，风格为圆形，旋转的
+                pd1.setTitle("提示");// 设置ProgressDialog 标题
+                pd1.setMessage("这是一个条状进度条对话框");// 设置ProgressDialog提示信息
+                //pd1.setIcon(R.drawable.secondback);// 设置ProgressDialog标题图标
+                // 设置ProgressDialog 的进度条是否不明确 false 就是不设置为不明确
+                pd1.setIndeterminate(false);
+                pd1.setCancelable(true); // 设置ProgressDialog 是否可以按退回键取消
+                pd1.setProgress(100);// 设置ProgressDialog 进度条进度
+                pd1.show(); // 让ProgressDialog显示
+                count = 0;
+                new Thread() {
+                    public void run() {
+                        try {
+                            while(count <= 100) {
+                                // 由线程来控制进度
+                                pd1.setProgress(count++);
+                                Thread.sleep(100);
+                            }
+                            pd1.cancel();
+                        } catch (Exception e) {
+                            pd1.cancel();
+                        }
+                    }
+                }.start();
             }
         }
     };
